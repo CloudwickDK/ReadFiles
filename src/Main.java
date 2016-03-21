84d11fc87a7b7970786e3bc84f5329b6c3346cf2
Despoina Karna <falsedk@hotmail.com>
2016-03-21 00:46:40 +0000

    Working for multiple phones

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import readJson.ReadJson;
 import readcsv.ReadCSV;
 
@@ -14,30 +17,33 @@ public class Main {
 
 		ReadCSV csv_obj = new ReadCSV();
 		ReadJson json_obj = new ReadJson();
-		
+
 		Reader csv_reader = null;
 		Reader json_reader = null;
 		Writer writer = null;
 		try {
-			//readers
+			// readers
 			csv_reader = new FileReader("files/data.csv");
 			json_reader = new FileReader("files/data.json");
-			//writers
+			// writers
 			writer = new FileWriter("files/written_data.json");
-			//read the files
+			// read the files
 			csv_obj.read(csv_reader);
 			readJson.Employee employee = json_obj.read(json_reader);
-			
-			//write
-			System.out.println(employee);
-			//json_obj.write(writer, employee);
-			
+
+			// write
+			//System.out.println(employee);
+			// json_obj.write(writer, employee);
+
+			Gson gson = new GsonBuilder().create();
+			System.out.println(gson.toJson(employee));
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			try { //close the readers
+			try { // close the readers
 				if (csv_reader != null) {
 					csv_reader.close();
 				}
--- src/readJson/ReadJson.java
+++ src/readJson/ReadJson.java
@@ -34,6 +34,4 @@ public class ReadJson {
 		
 		return 0;
 	}
-
-	
 }
